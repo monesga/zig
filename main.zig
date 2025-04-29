1588aecc5cc5df0a4cda7fafe791f0ceac417b5b
Mohsen <mohsen@velentir.com>
2025-04-29 15:25:15 +0500

    simplified

--- main.zig
+++ main.zig
@@ -18,28 +18,6 @@ pub fn main() !void {
     try bw.flush(); // Don't forget to flush!
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 test "var const" {
     var a: i32 = 1;
     const b: i32 = 2;
@@ -49,7 +27,6 @@ test "var const" {
 
 test "undefined" {
     var a: i32 = undefined;
-
     a = 1;
 }
 
@@ -117,6 +94,3 @@ test "array replicate" {
 }
 
 const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zig_lib");

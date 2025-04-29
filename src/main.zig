81fc0f39c57432b2654d51f1402712b77c96c99a
Mohsen <mohsen@velentir.com>
2025-04-29 14:48:18 +0500

    more tutorials

--- src/main.zig
+++ src/main.zig
@@ -65,6 +65,57 @@ test "var mutate" {
     _ = b;
 }
 
+test "array declaration" {
+    const a1 = [4]u8{ 1, 2, 3, 4 };
+    const a2 = [_]u8{ 1, 2, 3, 4 };
+    _ = a1;
+    _ = a2;
+}
+
+test "array indexing" {
+    const a1 = [4]u8{ 1, 2, 3, 4 };
+    const b = a1[2];
+    try std.testing.expectEqual(@as(u8, 3), b);
+}
+
+test "slice" {
+    const a1 = [4]u8{ 1, 2, 3, 4 };
+    const b = a1[1..3];
+    try std.testing.expectEqual(@as(u8, 2), b[0]);
+    try std.testing.expectEqual(@as(u8, 3), b[1]);
+    try std.testing.expectEqual(@as(u32, 2), b.len);
+    const c = a1[1..];
+    try std.testing.expectEqual(@as(u8, 2), c[0]);
+    try std.testing.expectEqual(@as(u8, 3), c[1]);
+    try std.testing.expectEqual(@as(u8, 4), c[2]);
+    try std.testing.expectEqual(@as(u32, 3), c.len);
+}
+
+test "array concat" {
+    const a1 = [4]u8{ 1, 2, 3, 4 };
+    const a2 = [4]u8{ 5, 6, 7, 8 };
+    const b = a1 ++ a2;
+    try std.testing.expectEqual(@as(u8, 1), b[0]);
+    try std.testing.expectEqual(@as(u8, 2), b[1]);
+    try std.testing.expectEqual(@as(u8, 3), b[2]);
+    try std.testing.expectEqual(@as(u8, 4), b[3]);
+    try std.testing.expectEqual(@as(u8, 5), b[4]);
+    try std.testing.expectEqual(@as(u8, 6), b[5]);
+    try std.testing.expectEqual(@as(u8, 7), b[6]);
+    try std.testing.expectEqual(@as(u8, 8), b[7]);
+    try std.testing.expectEqual(@as(u32, 8), b.len);
+}
+
+test "array replicate" {
+    const a = [_]u8{ 1, 2 };
+    const b = a ** 2;
+    try std.testing.expectEqual(@as(u8, 1), b[0]);
+    try std.testing.expectEqual(@as(u8, 2), b[1]);
+    try std.testing.expectEqual(@as(u8, 1), b[2]);
+    try std.testing.expectEqual(@as(u8, 2), b[3]);
+    try std.testing.expectEqual(@as(u32, 4), b.len);
+}
+
 const std = @import("std");
 
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.

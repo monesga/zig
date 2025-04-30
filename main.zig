c995395f26861de429948530c149bfcd38835d6d
Mohsen <mohsen@velentir.com>
2025-04-30 16:26:41 +0500

    type casting

--- main.zig
+++ main.zig
@@ -4,6 +4,8 @@
 
 const std = @import("std");
 const builtin = @import("builtin");
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
@@ -53,45 +55,45 @@ test "array declaration" {
 test "array indexing" {
     const a1 = [4]u8{ 1, 2, 3, 4 };
     const b = a1[2];
-    try std.testing.expectEqual(@as(u8, 3), b);
+    try expectEqual(@as(u8, 3), b);
 }
 
 test "slice" {
     const a1 = [4]u8{ 1, 2, 3, 4 };
     const b = a1[1..3];
-    try std.testing.expectEqual(@as(u8, 2), b[0]);
-    try std.testing.expectEqual(@as(u8, 3), b[1]);
-    try std.testing.expectEqual(@as(u32, 2), b.len);
+    try expectEqual(@as(u8, 2), b[0]);
+    try expectEqual(@as(u8, 3), b[1]);
+    try expectEqual(@as(u32, 2), b.len);
     const c = a1[1..];
-    try std.testing.expectEqual(@as(u8, 2), c[0]);
-    try std.testing.expectEqual(@as(u8, 3), c[1]);
-    try std.testing.expectEqual(@as(u8, 4), c[2]);
-    try std.testing.expectEqual(@as(u32, 3), c.len);
+    try expectEqual(@as(u8, 2), c[0]);
+    try expectEqual(@as(u8, 3), c[1]);
+    try expectEqual(@as(u8, 4), c[2]);
+    try expectEqual(@as(u32, 3), c.len);
 }
 
 test "array concat" {
     const a1 = [4]u8{ 1, 2, 3, 4 };
     const a2 = [4]u8{ 5, 6, 7, 8 };
     const b = a1 ++ a2;
-    try std.testing.expectEqual(@as(u8, 1), b[0]);
-    try std.testing.expectEqual(@as(u8, 2), b[1]);
-    try std.testing.expectEqual(@as(u8, 3), b[2]);
-    try std.testing.expectEqual(@as(u8, 4), b[3]);
-    try std.testing.expectEqual(@as(u8, 5), b[4]);
-    try std.testing.expectEqual(@as(u8, 6), b[5]);
-    try std.testing.expectEqual(@as(u8, 7), b[6]);
-    try std.testing.expectEqual(@as(u8, 8), b[7]);
-    try std.testing.expectEqual(@as(u32, 8), b.len);
+    try expectEqual(@as(u8, 1), b[0]);
+    try expectEqual(@as(u8, 2), b[1]);
+    try expectEqual(@as(u8, 3), b[2]);
+    try expectEqual(@as(u8, 4), b[3]);
+    try expectEqual(@as(u8, 5), b[4]);
+    try expectEqual(@as(u8, 6), b[5]);
+    try expectEqual(@as(u8, 7), b[6]);
+    try expectEqual(@as(u8, 8), b[7]);
+    try expectEqual(@as(u32, 8), b.len);
 }
 
 test "array replicate" {
     const a = [_]u8{ 1, 2 };
     const b = a ** 2;
-    try std.testing.expectEqual(@as(u8, 1), b[0]);
-    try std.testing.expectEqual(@as(u8, 2), b[1]);
-    try std.testing.expectEqual(@as(u8, 1), b[2]);
-    try std.testing.expectEqual(@as(u8, 2), b[3]);
-    try std.testing.expectEqual(@as(u32, 4), b.len);
+    try expectEqual(@as(u8, 1), b[0]);
+    try expectEqual(@as(u8, 2), b[1]);
+    try expectEqual(@as(u8, 1), b[2]);
+    try expectEqual(@as(u8, 2), b[3]);
+    try expectEqual(@as(u32, 4), b.len);
 }
 
 test "dynamic slice" {
@@ -109,7 +111,7 @@ test "dynamic slice" {
     const buffer = try allocator.alloc(u8, n);
     defer allocator.free(buffer);
     const slice = buffer[0..];
-    try std.testing.expectEqual(@as(usize, n), slice.len);
+    try expectEqual(@as(usize, n), slice.len);
 }
 
 test "blocks" {
@@ -118,17 +120,17 @@ test "blocks" {
         y += 1;
         break :add_one y;
     };
-    try std.testing.expectEqual(@as(i32, 124), x);
-    try std.testing.expectEqual(@as(i32, 124), y);
+    try expectEqual(@as(i32, 124), x);
+    try expectEqual(@as(i32, 124), y);
 }
 
 test "basic strings" {
     const stdout = std.io.getStdErr().writer();
     const str: []const u8 = "Hello, world!";
-    try std.testing.expectEqual(@as(u32, 13), str.len);
-    try std.testing.expectEqual(@as(u8, 'H'), str[0]);
-    try std.testing.expectEqual(@as(u8, 'o'), str[4]);
-    try std.testing.expectEqual(@as(u8, '!'), str[12]);
+    try expectEqual(@as(u32, 13), str.len);
+    try expectEqual(@as(u8, 'H'), str[0]);
+    try expectEqual(@as(u8, 'o'), str[4]);
+    try expectEqual(@as(u8, '!'), str[12]);
     try stdout.print(
         "<<string: {s} ",
         .{str},
@@ -152,18 +154,18 @@ test "string hex loop" {
 test "string length" {
     const str = "0123456789012345678901234567890123";
     const len = str.len;
-    try std.testing.expectEqual(@as(u32, 34), len);
+    try expectEqual(@as(u32, 34), len);
 }
 
 test "@TypeOf" {
     const arr = [_]u8{ 1, 2, 3, 4 };
-    try std.testing.expectEqual(@TypeOf(arr), [4]u8);
+    try expectEqual(@TypeOf(arr), [4]u8);
 
     const str = "ABC";
-    try std.testing.expectEqual(@TypeOf(str), *const [3:0]u8);
+    try expectEqual(@TypeOf(str), *const [3:0]u8);
 
     const pa = &arr;
-    try std.testing.expectEqual(@TypeOf(pa), *const [4]u8);
+    try expectEqual(@TypeOf(pa), *const [4]u8);
 
     const stdout = std.io.getStdErr().writer();
     try stdout.print("<<arr:{}, str:{}, pa:{}>>", .{ @TypeOf(arr), @TypeOf(str), @TypeOf(pa) });
@@ -173,21 +175,21 @@ test "UTF-8 raw codepoint" {
     const str = "Ⱥ";
     const c0 = str[0];
     const c1 = str[1];
-    try std.testing.expectEqual(@as(u8, 0xC8), c0);
-    try std.testing.expectEqual(@as(u8, 0xBA), c1);
-    try std.testing.expectEqual(@as(usize, 2), str.len);
+    try expectEqual(@as(u8, 0xC8), c0);
+    try expectEqual(@as(u8, 0xBA), c1);
+    try expectEqual(@as(usize, 2), str.len);
 }
 
 test "UTF-8 view" {
     var utf8 = (try std.unicode.Utf8View.init("Ⱥ")).iterator();
 
     const codepoint = utf8.nextCodepoint();
-    try std.testing.expectEqual(@as(u21, 570), codepoint);
+    try expectEqual(@as(u21, 570), codepoint);
 }
 
 test "string equality " {
     const name: []const u8 = "Mohsen";
-    try std.testing.expectEqual(
+    try expectEqual(
         true,
         std.mem.eql(u8, name, "Mohsen"),
     );
@@ -201,7 +203,7 @@ test "string concat" {
         "world!",
     });
     defer allocator.free(concat);
-    try std.testing.expectEqual(
+    try expectEqual(
         true,
         std.mem.eql(u8, concat, "Hello, world!"),
     );
@@ -209,11 +211,11 @@ test "string concat" {
 
 test "string startsWith" {
     const str = "Hello, world!";
-    try std.testing.expectEqual(
+    try expectEqual(
         true,
         std.mem.startsWith(u8, str, "Hello"),
     );
-    try std.testing.expectEqual(
+    try expectEqual(
         false,
         std.mem.startsWith(u8, str, "world"),
     );
@@ -223,11 +225,11 @@ test "string replace" {
     const str = "Hello";
     var buffer: [5]u8 = undefined;
     const nrep = std.mem.replace(u8, str, "el", "37", buffer[0..]);
-    try std.testing.expectEqual(
+    try expectEqual(
         true,
         std.mem.eql(u8, &buffer, "H37lo"),
     );
-    try std.testing.expectEqual(
+    try expectEqual(
         @as(u32, 1),
         nrep,
     );
@@ -242,7 +244,7 @@ test "if statement" {
     } else {
         a = 2;
     }
-    try std.testing.expectEqual(@as(i32, 1), a);
+    try expectEqual(@as(i32, 1), a);
 }
 
 test "switch statement" {
@@ -262,8 +264,8 @@ test "switch statement" {
         },
     }
 
-    try std.testing.expectEqual(@as(u32, 6), desc.len);
-    try std.testing.expectEqual(true, std.mem.eql(u8, desc, "Mammal"));
+    try expectEqual(@as(u32, 6), desc.len);
+    try expectEqual(true, std.mem.eql(u8, desc, "Mammal"));
 }
 
 test "switch else" {
@@ -275,7 +277,7 @@ test "switch else" {
         4, 5, 6 => y = 2,
         else => y = 3,
     }
-    try std.testing.expectEqual(@as(i32, 3), y);
+    try expectEqual(@as(i32, 3), y);
 }
 
 test "switch value" {
@@ -285,7 +287,7 @@ test "switch value" {
         4, 5, 6 => "two",
         else => "three",
     };
-    try std.testing.expectEqual(std.mem.eql(u8, desc, "three"), true);
+    try expectEqual(std.mem.eql(u8, desc, "three"), true);
 }
 
 test "switch ranges" {
@@ -299,7 +301,7 @@ test "switch ranges" {
         51...60 => "six",
         else => "seven",
     };
-    try std.testing.expectEqual(std.mem.eql(u8, desc, "seven"), true);
+    try expectEqual(std.mem.eql(u8, desc, "seven"), true);
 }
 
 test "labeled switch" {
@@ -311,7 +313,7 @@ test "labeled switch" {
         3 => val = 4,
         else => val = 5,
     }
-    try std.testing.expectEqual(@as(u8, 4), val);
+    try expectEqual(@as(u8, 4), val);
 }
 
 test "defer" {
@@ -322,7 +324,7 @@ test "defer" {
         defer a *= 3; // Last in first out
         a += 1;
     }
-    try std.testing.expectEqual(@as(i32, 4), a);
+    try expectEqual(@as(i32, 4), a);
 }
 
 test "for loop" {
@@ -331,7 +333,7 @@ test "for loop" {
     for (arr) |i| {
         sum += i;
     }
-    try std.testing.expectEqual(@as(i32, 10), sum);
+    try expectEqual(@as(i32, 10), sum);
 }
 
 test "for index and value" {
@@ -340,7 +342,7 @@ test "for index and value" {
     for (arr, 0..) |i, j| {
         sum += i + j;
     }
-    try std.testing.expectEqual(@as(usize, 16), sum);
+    try expectEqual(@as(usize, 16), sum);
 }
 
 test "while loop" {
@@ -348,7 +350,7 @@ test "while loop" {
     while (i < 10) {
         i += 1;
     }
-    try std.testing.expectEqual(@as(i32, 10), i);
+    try expectEqual(@as(i32, 10), i);
 }
 
 test "while loop with increment expression" {
@@ -357,8 +359,8 @@ test "while loop with increment expression" {
     while (i < 10) : (i += 1) {
         j += 1;
     }
-    try std.testing.expectEqual(@as(i32, 10), i);
-    try std.testing.expectEqual(@as(i32, 10), j);
+    try expectEqual(@as(i32, 10), i);
+    try expectEqual(@as(i32, 10), j);
 }
 
 test "break" {
@@ -369,7 +371,7 @@ test "break" {
             break;
         }
     }
-    try std.testing.expectEqual(@as(i32, 10), i);
+    try expectEqual(@as(i32, 10), i);
 }
 
 test "continue" {
@@ -381,7 +383,7 @@ test "continue" {
         }
         j += i;
     }
-    try std.testing.expectEqual(@as(i32, 1 + 3 + 5 + 7 + 9), j);
+    try expectEqual(@as(i32, 1 + 3 + 5 + 7 + 9), j);
 }
 
 fn add2(x: *i32) void {
@@ -391,7 +393,7 @@ fn add2(x: *i32) void {
 test "reference and dereference" {
     var a: i32 = 1;
     add2(&a);
-    try std.testing.expectEqual(@as(i32, 3), a);
+    try expectEqual(@as(i32, 3), a);
 }
 
 const Point = struct {
@@ -404,11 +406,11 @@ const Point = struct {
 };
 test "struct basics" {
     const p1 = Point{ .x = 1, .y = 2 };
-    try std.testing.expectEqual(@as(i32, 1), p1.x);
-    try std.testing.expectEqual(@as(i32, 2), p1.y);
+    try expectEqual(@as(i32, 1), p1.x);
+    try expectEqual(@as(i32, 2), p1.y);
     const p2 = Point.init(3, 4);
-    try std.testing.expectEqual(@as(i32, 3), p2.x);
-    try std.testing.expectEqual(@as(i32, 4), p2.y);
+    try expectEqual(@as(i32, 3), p2.x);
+    try expectEqual(@as(i32, 4), p2.y);
 }
 
 const Vec3 = struct {
@@ -441,16 +443,23 @@ test "self" {
     const p1 = Vec3{ .x = 1, .y = 2, .z = 3 };
     const p2 = Vec3{ .x = 4, .y = 5, .z = 6 };
     const d = p1.distance(p2);
-    try std.testing.expectEqual(@as(f64, 5.196152422706632), d);
+    try expectEqual(@as(f64, 5.196152422706632), d);
     var p3 = Vec3{ .x = 1, .y = 2, .z = 3 };
     p3.double();
-    try std.testing.expectEqual(@as(f64, 2), p3.x);
-    try std.testing.expectEqual(@as(f64, 4), p3.y);
-    try std.testing.expectEqual(@as(f64, 6), p3.z);
+    try expectEqual(@as(f64, 2), p3.x);
+    try expectEqual(@as(f64, 4), p3.y);
+    try expectEqual(@as(f64, 6), p3.z);
 }
 
 test "type inference with dot" {
     const Fruit = enum { Apple, Orange, Banana };
     const fruit: Fruit = .Apple;
-    try std.testing.expectEqual(.Apple, fruit);
+    try expectEqual(.Apple, fruit);
+}
+
+test "type casting with as" {
+    const a: usize = 65535;
+    const b: u32 = @as(u32, a);
+    try expectEqual(@as(u32, 65535), b);
+    try expectEqual(@TypeOf(b), u32);
 }

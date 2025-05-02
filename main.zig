1cd97afd90f58a2cbb5bf60e4bd2caef94eada83
Mohsen <mohsen@velentir.com>
2025-05-02 16:33:01 +0500

    more http stuff

--- main.zig
+++ main.zig
@@ -31,7 +31,8 @@ pub fn main() !void {
         buffer[i] = 0;
     }
     _ = try read_request(connection, buffer[0..buffer.len]);
-    std.debug.print("Buffer: {s}\n", .{buffer});
+    const request = try parse_request(buffer[0..]);
+    std.debug.print("Request: {any}\n", .{request});
 }
 
 test "var const" {
@@ -847,4 +848,39 @@ const Connection = std.net.Server.Connection;
 pub fn read_request(conn: Connection, buffer: []u8) !void {
     const reader = conn.stream.reader();
     _ = try reader.read(buffer);
+}
+
+const Method = enum {
+    GET,
+
+    pub fn init(text: []const u8) !Method {
+        return MethodMap.get(text).?;
+    }
+
+    pub fn is_supported(m: []const u8) bool {
+        return MethodMap.contains(m);
+    }
+};
+
+const Map = std.static_string_map.StaticStringMap;
+const MethodMap = Map(Method).initComptime(.{.{ "GET", .GET }});
+
+const Request = struct {
+    method: Method,
+    version: []const u8,
+    uri: []const u8,
+    pub fn init(method: Method, uri: []const u8, version: []const u8) Request {
+        return Request{ .method = method, .uri = uri, .version = version };
+    }
+};
+
+pub fn parse_request(text: []u8) !Request {
+    const line_index = std.mem.indexOfScalar(u8, text, '\n') orelse text.len;
+    var iterator = std.mem.splitScalar(u8, text[0..line_index], ' ');
+    const method_str = iterator.next();
+    const method = try Method.init(method_str.?);
+    const uri = iterator.next().?;
+    const version = iterator.next().?;
+    const request = Request.init(method, uri, version);
+    return request;
 }

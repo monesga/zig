5f96905fdc47017bec46bb6dc4e1cc1452ef9125
Mohsen <mohsen@velentir.com>
2025-05-03 13:16:19 +0500

    more build stuff

--- build.zig
+++ build.zig
@@ -13,4 +13,15 @@ pub fn build(b: *std.Build) void {
     const run_arti = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the executable");
     run_step.dependOn(&run_arti.step);
+
+    const test_exe = b.addTest(.{
+        .name = "unit_tests",
+        .root_source_file = b.path("main.zig"),
+        .target = b.graph.host,
+        .optimize = .Debug, // .ReleaseSafe,
+    });
+    b.installArtifact(test_exe);
+    const test_arti = b.addRunArtifact(test_exe);
+    const test_step = b.step("test", "Run the tests");
+    test_step.dependOn(&test_arti.step);
 }
